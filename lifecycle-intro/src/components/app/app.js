d51d7e7affc9c0677fa537b93c84651cac806b6f
Max Krasilnik <maxkrasilnik@gmail.com>
2022-01-21 16:21:36 +0200

    Final versions of projects by React/Redux course

---
+++ lifecycle-intro/src/components/app/app.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ErrorButton from '../error-button';
+import PeoplePage from '../people-page';
+
+import './app.css';
+import ErrorIndicator from '../error-indicator';
+
+export default class App extends Component {
+
+  state = {
+    showRandomPlanet: true,
+    hasError: false
+  };
+
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
+
+
+  componentDidCatch(){
+    console.log('componentDidCatch()');
+    this.setState({hasError: true});
+  }
+
+  render() {
+
+    if(this.state.hasError){
+      return <ErrorIndicator/>
+    }
+
+    const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
+
+    return (
+      <div className="stardb-app">
+        <Header />
+        { planet }
+
+        <div className='row mb2 button-row'>
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+          <ErrorButton/>
+        </div>
+        
+        <PeoplePage/>
+        <PeoplePage/>
+        <PeoplePage/>
+      </div>
+    );
+  }
+}
---
+++ todo/src/components/search-panel/search-panel.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './search-panel.css';
+
+export default class SearchPanel extends Component{
+  state = {
+    term: ''
+  };
+
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({term});
+    this.props.onSearchChange(term);
+  };
+
+  render(){
+    const searchText = 'Type here to search';
+    const searchStyle = {
+      fontSize: '25px'
+    }
+    return <input className='search-input' style={searchStyle} placeholder={searchText}
+              value={this.state.term} onChange={this.onSearchChange} />;
+  }
+};
