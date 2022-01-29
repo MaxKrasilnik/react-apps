d83fb9484be6061f1dbe170e9687c1bda48941d7
Max Krasilnik <maxkrasilnik@gmail.com>
2022-01-29 20:55:21 +0200

    Finished project "re-store"

--- re-store/src/services/bookstore-service.js
+++ re-store/src/services/bookstore-service.js
@@ -4,14 +4,14 @@ export default class BookstoreService {
             id: 1,
             title: 'Production-Ready Microservices',
             author: 'Sussan J.Fowler',
-            price: 32,
+            price: 20,
             coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'
         },
         {
             id: 2,
             title: 'Release It!',
             author: 'Michael T. Nygard',
-            price: 45,
+            price: 50,
             coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'
         }
     ];
--- re-store/src/store.js
+++ re-store/src/store.js
@@ -1,6 +1,32 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from './reducers';
+import thunkMiddleware from 'redux-thunk';
+
+const logMiddleware = (store) => (next) => (action) => {
+    console.log('logMiddleware');
+    console.log(action.type);
+    return next(action);
+};
+
+const stringMiddleware = () => (next) => (action) => {
+    console.log('stringMiddleware'); 
+    if(typeof action === 'string'){
+        return next({
+            type: action
+        });
+    }
+} 
+
+//const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+//const store = createStore(reducer, applyMiddleware(thunkMiddleware, stringMiddleware, logMiddleware));
+
+const delayedActionCreator = (timeout) => (dispatch) => {
+    setTimeout(() => dispatch({
+        type: 'DELAYED_ACTION'
+    }), timeout);
+};
+
+//store.dispatch(delayedActionCreator(3000));
 
 const store = createStore(reducer);
-
 export default store;
